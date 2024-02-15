95b6799644598e055750d25e0ec79150b8ee2037
nantege elizabeth betty <nanelizabet2020@gmail.com>
2024-02-15 10:48:20 +0300

    Update trail.py

--- trail.py
+++ trail.py
@@ -1,1 +1,24 @@
 print("hello world")
+number = -1
+if number >0:
+
+    print("number is positive\n")
+
+else:
+
+    print ('number is negative')
+
+numbers = [10,12,14,'liz']
+names = []
+print(numbers)
+print(numbers[3]) 
+print(len(numbers))
+del numbers[1]
+print(numbers)
+numbers.append(48)
+numbers.insert(2,'betty')
+print(numbers)
+class_names=['liz','betty','bob','rick','jo']
+class_attendance=['liz','betty']
+new_list=[]
+choped_lesson=[]
